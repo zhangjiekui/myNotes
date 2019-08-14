cb553812147f39d8953e647878278734e3be44d2
zhangjiekui <33198334+zhangjiekui@users.noreply.github.com>
2019-08-14 12:15:14 +0800

    inner_lambda_partial 函数的用法

---
+++ custom_fastai/inner_lambda_partial.py
@@ -0,0 +1,39 @@
+# 业务需要两个甚至多个参数，但函数定义只接受一个参数
+
+# 函数定义只接受一个参数
+def slow_calculation(cb=None):
+    res = 0
+    for i in range(5):
+        res += i*i
+        sleep(1)
+        if cb: cb(i)
+    return res
+    
+ # 业务需要两个甚至多个参数
+def show_progress(exclamation:str,epoch):  #exclamation:感叹语
+    print(f" {exclamation}{epoch}")
+    
+    
+ # 三种解决办法
+def show_progress(exclamation:str,epoch):  #exclamation:感叹语
+    print(f"{exclamation} {epoch} ")
+
+def inner_show_progress(exclamation):
+    def _inner_function(epoch):
+        print(exclamation,epoch)
+    return _inner_function
+
+def lam_show_progress(exclamation):
+    _inner_lambda= lambda epoch: print(f"{exclamation}:{epoch}")
+    return _inner_lambda
+       
+from functools import partial
+_partial_func=partial(show_progress,"使用partial函数：")  
+
+_inner_func=inner_show_progress("使用内部函数：")
+_inner_lamfunc=lam_show_progress("使用lambda函数：")
+#-----------------------------------------------------
+
+slow_calculation(_partial_func)
+slow_calculation(_inner_func)
+slow_calculation(_inner_lamfunc)
