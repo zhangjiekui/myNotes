f9e99961c905291253e56b88eca65773be6999f0
zhangjiekui <33198334+zhangjiekui@users.noreply.github.com>
2019-09-29 22:21:46 +0800

    Update seq2seq_collate_func.py

--- custom_fastai/seq2seq/seq2seq_collate_func.py
+++ custom_fastai/seq2seq/seq2seq_collate_func.py
@@ -1,4 +1,4 @@
-def seq2seq_collate(samples, pad_idx=0, pad_first=False, backwards=True):
+def seq2seq_collate(samples, pad_idx=0, pad_first=True, backwards=True):
     "Function that collect samples and adds padding. Flips token order if needed"
     samples = to_data(samples)
     max_len_x,max_len_y = max([len(s[0]) for s in samples]),max([len(s[1]) for s in samples])
@@ -10,6 +10,7 @@ def seq2seq_collate(samples, pad_idx=0, pad_first=False, backwards=True):
             # res_x 是二维的，i代表的样本的index
             res_x[i,-len(s[0]):],res_y[i,-len(s[1]):] = LongTensor(s[0]),LongTensor(s[1])
         else:         
-            res_x[i,:len(s[0]):],res_y[i,:len(s[1]):] = LongTensor(s[0]),LongTensor(s[1])
+            res_x[i,:len(s[0])],res_y[i,:len(s[1])] = LongTensor(s[0]),LongTensor(s[1])
+            #res_x[i,:len(s[0]):],res_y[i,:len(s[1]):] = LongTensor(s[0]),LongTensor(s[1])
     if backwards: res_x,res_y = res_x.flip(1),res_y.flip(1)
     return res_x,res_y
