e7e6f7ed40ecf15f1b706976062befa4c9c40b2e
zhangjiekui <33198334+zhangjiekui@users.noreply.github.com>
2019-09-29 22:19:13 +0800

    course-nlp-master/7-seq2seq-translation.ipynb
    def seq2seq_collate(samples, pad_idx=0, pad_first=False, backwards=True)

---
+++ custom_fastai/seq2seq/seq2seq_collate_func.py
@@ -0,0 +1,15 @@
+def seq2seq_collate(samples, pad_idx=0, pad_first=False, backwards=True):
+    "Function that collect samples and adds padding. Flips token order if needed"
+    samples = to_data(samples)
+    max_len_x,max_len_y = max([len(s[0]) for s in samples]),max([len(s[1]) for s in samples])
+    res_x = torch.zeros(len(samples), max_len_x).long() + pad_idx
+    res_y = torch.zeros(len(samples), max_len_y).long() + pad_idx
+    if backwards: pad_first = not pad_first
+    for i,s in enumerate(samples):
+        if pad_first: 
+            # res_x 是二维的，i代表的样本的index
+            res_x[i,-len(s[0]):],res_y[i,-len(s[1]):] = LongTensor(s[0]),LongTensor(s[1])
+        else:         
+            res_x[i,:len(s[0]):],res_y[i,:len(s[1]):] = LongTensor(s[0]),LongTensor(s[1])
+    if backwards: res_x,res_y = res_x.flip(1),res_y.flip(1)
+    return res_x,res_y
